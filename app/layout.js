698baf342c9f70b2dbe5bfbcd22a9f9e01e0b451
maxbezs <baks.bezsmertnuy@gmail.com>
2024-07-16 04:18:54 +0200

    1

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,6 @@ import "./globals.css";
 import localFont from "next/font/local";
 import { Montserrat, Open_Sans } from "next/font/google";
 import GoogleAnalytics from "./components/GoogleAnalytics";
-import Script from "next/script";
 
 const openSans = Open_Sans({
   subsets: ["latin"],
@@ -58,24 +57,8 @@ export default function RootLayout({ children }) {
           content="https://zona-laser.vercel.app/depilacja-mezczyzn.jpg"
         />
       </head>
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-BZTSF0RX5Q"
-      ></script>
-      <Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments)};
-      gtag("js", new Date());
-      gtag("config", "G-BZTSF0RX5Q");
-    `,
-        }}
-      />
+      <GoogleAnalytics />
       <body>
-        {" "}
-        <GoogleAnalytics />
         {children}
         <Footer />
       </body>
